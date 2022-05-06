c7596129bc4be9b3122cb7e9bba862fb548198b8
missionfloyd <missionfloyd@users.noreply.github.com>
2022-05-06 00:26:26 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
@@ -15,20 +16,23 @@ namespace CitiesSkylinesLauncher
         static void Main(string[] args)
         {
             var gameDirIndex = Array.IndexOf(args, "--gameDir");
+
             if (gameDirIndex == -1)
             {
                 var pathFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Paradox Interactive\launcherpath");
                 Directory.CreateDirectory(Path.GetDirectoryName(pathFile));
-                File.WriteAllText(pathFile, Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\\n");
+                File.WriteAllText(pathFile, Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\\n");
                 MessageBox.Show("Launcher path set.\n\nStart the game as usual.", "", MessageBoxButton.OK, MessageBoxImage.Information);
-                return;
             }
-            var gameDir = args[gameDirIndex + 1];
-            var json = new JavaScriptSerializer();
-            var settings = json.Deserialize<LauncherSettings>(File.ReadAllText(Path.Combine(gameDir, "launcher-settings.json")));
-            var argList = new List<string>(args);
-            argList.RemoveRange(gameDirIndex, 2);
-            Process.Start(Path.Combine(gameDir, settings.exePath), string.Join(" ", argList));
+            else
+            {
+                var gameDir = args[gameDirIndex + 1];
+                var json = new JavaScriptSerializer();
+                var settings = json.Deserialize<LauncherSettings>(File.ReadAllText(Path.Combine(gameDir, "launcher-settings.json")));
+                var argList = new List<string>(args);
+                argList.RemoveRange(gameDirIndex, 2);
+                Process.Start(Path.Combine(gameDir, settings.exePath), string.Join(" ", argList));
+            }
         }
     }
 
